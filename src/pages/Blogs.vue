d47ae9a370ea017add027c5d149a1a8db7b421db
Hixie4 <jhixie4@gmail.com>
2019-07-31 11:28:29 -0700

    blogs and website pages added

---
+++ src/pages/Blogs.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+    <h1>All Blogs</h1>
+    <ul>
+        <li v-for="edge in $page.posts.edges" :key="edge.node.title"><a v-bind:href="edge.node.path">{{ edge.node.title }}</a></li>
+    </ul>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'List of blogs'
+  }
+}
+</script>
+
+
+<page-query>
+  query blogPosts {
+    posts: allWebPost (filter: { blogpost: { eq: true }}) {
+      edges {
+        node {
+          id
+          title
+          path
+          date (format: "D. MMMM YYYY")
+          description
+        }
+      }
+    }
+  }
+</page-query>
---
+++ src/pages/Websites.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+    <h1>All Websites</h1>
+    <ul>
+        <li v-for="edge in $page.posts.edges" :key="edge.node.title"><a v-bind:href="edge.node.path">{{ edge.node.title }}</a></li>
+    </ul>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'List of Websites'
+  }
+}
+</script>
+
+
+<page-query>
+  query websitePosts {
+    posts: allWebPost (filter: { website: { eq: true }}) {
+      edges {
+        node {
+          id
+          title
+          path
+          date (format: "D. MMMM YYYY")
+          description
+        }
+      }
+    }
+  }
+</page-query>
