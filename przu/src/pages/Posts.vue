8b5ff41ffa14bb239380d089daed1729639178b2
Hixie4 <jhixie4@gmail.com>
2019-07-31 10:25:02 -0700

    posts page and webpost template

---
+++ przu/src/pages/Posts.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+    <h1>Posts</h1>
+    <ul>
+        <li v-for="edge in $page.posts.edges" :key="edge.node.title"><a v-bind:href="edge.node.path">{{ edge.node.title }}</a></li>
+    </ul>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'List of posts'
+  }
+}
+</script>
+
+
+<page-query>
+  query Posts {
+    posts: allWebPost {
+      edges {
+        node {
+          id
+          title
+          path
+          date (format: "D. MMMM YYYY")
+          description
+        }
+      }
+    }
+  }
+</page-query>
