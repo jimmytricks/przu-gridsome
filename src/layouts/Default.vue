63157775d852c95c28ce93efe512aaede5664511
Hixie4 <jhixie4@gmail.com>
2019-07-31 13:12:36 -0700

    Latest blogs displaying, links to pages added

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -7,6 +7,9 @@
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about">About</g-link>
+        <g-link class="nav__link" to="/posts">Posts</g-link>
+        <g-link class="nav__link" to="/blogs">Blogs</g-link>
+        <g-link class="nav__link" to="/websites">Websites</g-link>
       </nav>
     </header>
     <slot/>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,16 +4,21 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
     
-    <h1>Hello, world!</h1>
+    <h1>Welcome to the PRZU</h1>
    
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
     </p>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+    <h2>Latest Blog Posts</h2>
+    <section class="blog-posts">
+      <li v-for="post in $page.posts.edges" v-bind:key="post.node.title">{{ post.node.title }} - {{ post.node.date }}</li>
+    </section>
+
+    <h2>Latest Website posts</h2>
+    <section class="website-posts">
+        <li v-for="post in $page.webposts.edges" v-bind:key="post.node.title">{{ post.node.title }} - {{ post.node.date }}</li>
+    </section>
 
   </Layout>
 </template>
@@ -25,6 +30,33 @@ export default {
   }
 }
 </script>
+
+<page-query>
+  query blogPosts {
+    posts: allWebPost (filter: { blogpost: { eq: true }}, sortBy: "date", order: DESC) {
+      edges {
+        node {
+          id
+          title
+          path
+          date (format: "D MMMM YYYY")
+          description
+        }
+      }
+    },
+    webposts: allWebPost (filter: { website: { eq: true }}, sortBy: "date", order: DESC) {
+      edges {
+        node {
+          id
+          title
+          path
+          date (format: "D MMMM YYYY")
+          description
+        }
+      }
+    }
+  }
+</page-query>
 
 <style>
 .home-links a {
