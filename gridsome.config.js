81f46592820fdd0e59acdfc22fccf92aa2073db4
Hixie4 <jhixie4@gmail.com>
2019-08-01 10:48:00 -0700

    support for global scss added

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,6 +4,18 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/scss/globals.scss')
+      ],
+    })
+}
+
 module.exports = {
   siteName: 'Gridsome',
   plugins: [
@@ -24,4 +36,12 @@ module.exports = {
   transformers: {
     remark: {}
   },
+  chainWebpack (config) {
+    // Load variables for all vue-files
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+	}
 }
