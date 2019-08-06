62b278a0ce23ee11f170b570d96225ee12d76d80
Hixie4 <jhixie4@gmail.com>
2019-08-06 12:26:24 -0700

    dynamic site and source added

--- posts/didyouwatchthegame.md
+++ posts/didyouwatchthegame.md
@@ -1,11 +1,13 @@
 ---
 title: DidYouWatchTheGame.com
 date: 2018-12-01 07:42:34
-description: "Built in vanilla JS, uses the NHL REST API to show upcoming fixtures, previous results and higlights and table standings for all NHL teams. "
+description: Built in vanilla JS, uses the NHL REST API to show upcoming fixtures, previous results and higlights and table standings for all NHL teams.
 slug: didyouwatchthegame
 website: true
+websiteurl: https://DidYouWatchTheGame.com
+sourceurl: https://https://github.com/jimmytricks/CanucksHockey
 tech: ["HTML", "CSS", "SCSS", "JavaScript"]
-featureimg: "../src/assets/img/sites/didyouwatchthegame.jpg"
+featureimg: ../src/assets/img/sites/didyouwatchthegame.jpg
 ---
 
 ## DidYouWatchTheGame.com
--- src/templates/WebPost.vue
+++ src/templates/WebPost.vue
@@ -1,23 +1,38 @@
 <template>
-    <layout>
-      <section class="page-text-content container posts-page py-12">
-        <h1 class="pb-0">{{ $page.post.title }}</h1>
-        <p class="py-0 text-sm">{{ $page.post.date }}</p>
-        <p class="pt-0 pb-6 text-sm post-info-last">Category: <a v-for="(tech, index) in $page.post.tech" v-bind:key="tech.id" href="#"><span v-if="index != 0 ">, </span>{{ tech }}</a></p>
-        <div class="content pt-4" v-html="$page.post.content">
+  <layout>
+    <section class="page-text-content container posts-page py-12">
+      <h1 class="pb-0">{{ $page.post.title }}</h1>
+      <p v-if="$page.post.tech.length" class="p-0 text-xs">
+        Category:
+        <span v-for="(tech, index) in $page.post.tech" v-bind:key="tech.id">
+          <span v-if="index != 0 ">,</span>
+          {{ tech }}
+        </span>
+      </p>
+      <p class="p-0 text-xs mb-4 pb-4 border-btm">Last updated: {{ $page.post.date }}</p>
+      <div v-if="$page.post.featureimg">
+        <a v-bind:href="$page.post.websiteurl" target="_blank">
+          <g-image :src="$page.post.featureimg" class="shadow-lg"/>
+        </a>
+        <div class="link-container flex justify-between mt-2 border-btm pb-6 mb-2">
+          <a v-bind:href="$page.post.websiteurl" target="_blank">View Site</a>
+          <a v-if="$page.post.sourceurl" v-bind:href="$page.post.sourceurl" target="_blank">View Source on GitHub</a>
         </div>
-      </section>
-    </layout>
+      </div>
+
+      <div class="content" v-html="$page.post.content"></div>
+    </section>
+  </layout>
 </template>
 
 <script>
 export default {
-  metaInfo () {
+  metaInfo() {
     return {
       title: this.$page.post.title
-    }
+    };
   }
-}
+};
 </script>
 
 <page-query>
@@ -27,14 +42,18 @@ export default {
         date (format: "D MMMM, YYYY")
         content
         tech
+        featureimg
+        path
+        websiteurl
+        sourceurl
         }
     }
 </page-query>
 
 <style lang="scss">
 
+.border-btm {
+  border-bottom: 1px solid #d4d4d4;
+}
 
-  .post-info-last {
-    border-bottom: 1px solid #D4D4D4;
-  }
 </style>
