e35351b8474e44136d5fb7c708c57baa729c7af1
Hixie4 <jhixie4@gmail.com>
2019-08-07 10:53:31 -0700

    netlify form added

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,7 +1,40 @@
 <template>
   <Layout>
     <section class="page-text-content container px-4 py-8">
-      <h1>Contact</h1>Contact me
+      <h1>Contact</h1>
+      <form
+        name="contact"
+        method="post"
+        v-on:submit.prevent="handleSubmit"
+        action="/success/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out:
+            <input name="bot-field" />
+          </label>
+        </p>
+        <div class="sender-info">
+          <div>
+            <label for="name" class="label">Your name</label>
+            <input type="text" name="name" v-model="formData.name" />
+          </div>
+          <div>
+            <label for="email">Your email</label>
+            <input type="email" name="email" v-model="formData.email" />
+          </div>
+        </div>
+
+        <div class="message-wrapper">
+          <label for="message">Message</label>
+          <textarea name="message" v-model="formData.message"></textarea>
+        </div>
+
+        <button type="submit">Submit form</button>
+      </form>
     </section>
   </Layout>
 </template>
@@ -10,6 +43,32 @@
 export default {
   metaInfo: {
     title: "About us"
+  },
+  data() {
+    return {
+      formData: {}
+    };
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        )
+        .join("&");
+    },
+    handleSubmit(e) {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: this.encode({
+          "form-name": e.target.getAttribute("name"),
+          ...this.formData
+        })
+      })
+        .then(() => this.$router.push("/success"))
+        .catch(error => alert(error));
+    }
   }
 };
 </script>
---
+++ src/pages/Success.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <section class="page-text-content container px-4 py-8">
+      <h1>Success!</h1>
+      <p>The email has been sent and I will get back to you shortly, thanks.
+      </p>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Success, email sent"
+  }
+};
+</script>
